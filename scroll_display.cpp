2991d9edf89ca52b74fd32808b5e79ad355799d7
Jonathan <jonathancw@me.com>
2014-03-01 18:24:13 -0800

    Added UNTESTED scroll display

--- scroll_display.cpp
+++ scroll_display.cpp
@@ -4,3 +4,286 @@
 
 #include "scroll_display.hpp"
 
+namespace
+{
+    template<class type1, class type2>
+    inline type2 conv(const type1& t1)
+    {
+        std::stringstream ss;
+        type2 t2;
+        ss<< t1;
+        ss>> t2;
+        return t2;
+    }
+    
+    
+}
+
+namespace scrollDisplay
+{
+    /* True/false based on whether it moved the window or not. */
+    bool scroll_display_class::scroll_up()
+    {
+        this->sync();
+        bool success(false);
+        if(this->wind.size > 0)
+        {
+            if(this->wind.beg > 0)
+            {
+                success = true;
+                this->wind.beg--;
+            }
+        }
+        return success;
+    }
+    
+    /* True/false based on whether it moved the window or not. */
+    bool scroll_display_class::scroll_down()
+    {
+        this->sync();
+        bool success(false);
+        if(this->wind.size > 0)
+        {
+            if(((unsigned)this->wind.beg + ((unsigned)this->wind.size - 1)) < this->display->size())
+            {
+                this->wind.beg++;
+                success = true;
+            }
+        }
+        return success;
+    }
+    
+    /* True/false based on whether it moved the window or not. */
+    bool scroll_display_class::scroll_pg_down()
+    {
+        this->sync();
+        bool success(false);
+        if(this->wind.size > 0)
+        {
+            if(((unsigned)this->wind.beg + (2 * ((unsigned)this->wind.size - 1))) < this->display->size())
+            {
+                success = true;
+                this->wind.beg += (this->wind.size - 1);
+            }
+        }
+        return success;
+    }
+    
+    /* True/false based on whether it moved the window or not. */
+    bool scroll_display_class::scroll_pg_up()
+    {
+        this->sync();
+        bool success(false);
+        if(this->wind.beg >= (this->wind.size - 1))
+        {
+            success = true;
+            this->wind.beg -= (this->wind.size - 1);
+        }
+        return success;
+    }
+    
+    std::vector<std::string> scroll_display_class::window()
+    {
+        this->sync();
+        using namespace std;
+        vector<string> tempv;
+        for(unsigned int x = this->wind.beg; 
+                ((x < (this->wind.beg + this->wind.size)) && (x < this->display->size())); x++)
+        {
+            tempv.push_back(this->display->at(x));
+        }
+        return tempv;
+    }
+    
+    /* Keeps the window within the bounds of the display. */
+    void scroll_display_class::sync()
+    {
+        //never ever use any member function if the display is null!!
+        assert(this->display != std::nullptr_t);
+        
+        //make sure the beg is within defined bounds
+        if(this->wind.size > 0)
+        {
+            if(((unsigned)this->wind.beg + ((unsigned)this->wind.size - 1)) >= this->display->size())
+            {
+                this->wind.beg = (this->display->size() - this->wind.size);
+            }
+            if(this->wind.beg < 0) this->wind.beg = 0;
+        }
+    }
+    
+    bool scroll_display_class::mv_up()
+    {
+        this->sync();
+        bool success(false);
+        if(this->display->size() > 0)
+        {
+            switch(this->pos.part == 0)
+            {
+                case true:
+                {
+                    success = this->scroll_up();
+                }
+                break;
+                
+                case false:
+                {
+                    this->part--;
+                }
+                break;
+                
+                default:
+                {
+                }
+                break;
+            }
+        }
+        this->pos.whole = (this->pos.part + this->wind.beg);
+        return success;
+    }
+    
+    bool scroll_display_class::mv_down()
+    {
+        this->sync();
+        bool success(false);
+        if(this->display->size() > 0)
+        {
+            switch(this->pos.part == (this->wind.size - 1))
+            {
+                case true:
+                {
+                    success = this->scroll_down();
+                }
+                break;
+                
+                case false:
+                {
+                    this->pos.part++;
+                }
+                break;
+                
+                default:
+                {
+                }
+                break;
+            }
+            this->pos.whole = (this->wind.beg + this->pos.part);
+        }
+        return success;
+    }
+    
+    bool scroll_display_class::pg_up()
+    {
+        this->sync();
+        bool success(false);
+        if(this->display->size() > 0)
+        {
+            switch(this->pos.part == (this->wind.size - 1))
+            {
+                case true:
+                {
+                    this->pos.part = 0;
+                    this->pos.whole = (this->wind.beg + this->pos.part);
+                }
+                break;
+                
+                case false:
+                {
+                    switch(this->pos.whole >= (this->wind.size - 1))
+                    {
+                        case true:
+                        {
+                            this->pos.whole -= (this->wind.size - 1);
+                            this->pos.part = 0;
+                            this->wind.beg = this->pos.whole;
+                            success = true;
+                        }
+                        break;
+                        
+                        case false:
+                        {
+                            this->pos.part = 0;
+                            this->pos.whole = 0;
+                            this->wind.beg = 0;
+                            success = true;
+                        }
+                        break;
+                        
+                        default:
+                        {
+                        }
+                        break;
+                    }
+                }
+                break;
+                
+                default:
+                {
+                }
+                break;
+            }
+        }
+        return success;
+    }
+    
+    bool scroll_display_class::pg_down()
+    {
+        this->sync();
+        bool success(false);
+        if(this->display->size() > 0)
+        {
+            if(this->pos.whole < this->display->size())
+            {
+                switch((this->pos.whole + (this->wind.size - 1)) < this->display->size())
+                {
+                    case true:
+                    {
+                        switch(this->pos.part == 0)
+                        {
+                            case true:
+                            {
+                                this->pos.part = (this->wind.size - 1);
+                                this->pos.whole = (this->wind.beg + this->pos.part);
+                            }
+                            break;
+                            
+                            case false:
+                            {
+                                this->pos.whole += (this->wind.size - 1);
+                                this->pos.part = (this->wind.size - 1);
+                                this->wind.beg = (this->pos.whole - (this->wind.size - 1));
+                                success = true;
+                            }
+                            break;
+                            
+                            default:
+                            {
+                            }
+                            break;
+                        }
+                        
+                        
+                    }
+                    break;
+                    
+                    case false:
+                    {
+                        this->pos.whole = (this->display->size() - this->wind.size);
+                        this->wind.beg = (this->display->size() - this->wind.size);
+                        this->pos.part = (this->wind.size - 1);
+                    }
+                    break;
+                    
+                    default:
+                    {
+                    }
+                    break;
+                }
+            }
+        }
+        return success;
+    }
+    
+    
+}
+
--- scroll_display.hpp
+++ scroll_display.hpp
@@ -1,6 +1,68 @@
 #ifndef SCROLL_DISPLAY_HPP_INCLUDED
 #define SCROLL_DISPLAY_HPP_INCLUDED
+#include <vector>
+#include <string>
 
+#include "global_defines.hpp"
+#include <assert.h>
 
+namespace scrollDisplay
+{
+    class scroll_display_class;
+    
+    struct window_data
+    {
+        int beg = 0;
+        signed long size = 15;
+    };
+    
+    struct position_data
+    {
+        short part = 0;
+        unsigned int whole = 0;
+    };
+    
+    /* scroll_display_class: a class designed to manage a window and make 
+     * it easy to create a scroll display.*/
+    class scroll_display_class
+    {
+    public:
+        explicit scroll_display_class() : display(NULL), wind(), pos() {}
+        explicit scroll_display_class(std::vector<std::string> d) : display(&d),
+                    wind(),
+                    pos()
+        {
+            //no one should have to give this thing a null pointer...
+            assert(d != std::nullptr_t);  //if you're using a null pointer, you're doing it wrong!
+        }
+        
+        ~scroll_display_class(){}
+        
+        bool mv_up();
+        bool mv_down();
+        bool pg_up();
+        bool pg_down();
+        
+        std::vector<std::string> window();
+        
+        long &window_size()
+        {
+            return this->wind.size;
+        }
+        
+    private:
+        std::vector<std::string> *display;
+        window_data wind;
+        position_data pos;
+        
+        void sync();
+        bool scroll_up();
+        bool scroll_down();
+        bool scroll_pg_up();
+        bool scroll_pg_down();
+    };
+    
+    
+}
 
 #endif
